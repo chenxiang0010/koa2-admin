4763513ac9d00207469c0adc5bc7056e55842d6a
chenxxx1783415 <1783415@qq.com>
2021-10-22 01:46:49 +0800

    2
    Signed-off-by: chenxxx1783415 <1783415@qq.com>

--- app.js
+++ app.js
@@ -7,32 +7,32 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const cors = require('koa2-cors')
 
-const routerResponse = require('./src/utils/result')
+const { routerResponse } = require('./src/utils/result')
 // error handler
 onerror(app)
 
 // middlewares
 app.use(
-	bodyparser({
-		enableTypes: ['json', 'form', 'text'],
-	})
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
 )
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(
-	views(__dirname + '/views', {
-		extension: 'ejs',
-	})
+  views(__dirname + '/views', {
+    extension: 'ejs',
+  })
 )
 
 // logger
 app.use(async (ctx, next) => {
-	const start = new Date()
-	await next()
-	const ms = new Date() - start
-	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 //统一响应处理
@@ -41,13 +41,17 @@ app.use(routerResponse())
 //cors
 app.use(cors())
 
+
+app.use(ctx => {
+  ctx.sendResult(null, 200, 'secc')
+})
 // routes
-const useRoutes = require('./src/routes/index')
-useRoutes(app)
+// const useRoutes = require('./src/routes/index')
+// useRoutes(app)
 
 // error-handling
 app.on('error', (err, ctx) => {
-	console.error('server error', err, ctx)
+  console.error('server error', err, ctx)
 })
 
 module.exports = app
